b8c39c2c45b6195010a4b939903987a5ad5deae0
Patrick McElwee <pmm21@duke.edu>
2014-04-01 00:45:40 -0400

    Order#contains? is no longer a finder

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -35,7 +35,7 @@ Spree::Order.class_eval do
   end
 
   def add_variant(variant, quantity = 1)
-    current_item = contains?(variant)
+    current_item = find_line_item_by_variant(variant)
     if current_item
       current_item.quantity += quantity
       current_item.save
