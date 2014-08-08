355008b03722cb2abacb48a8c7047b34a4cf496a
Patrick McElwee <pmm21@duke.edu>
2014-08-08 16:35:09 -0400

    Preserve original LineItem #copy_price

--- app/models/spree/line_item_decorator.rb
+++ app/models/spree/line_item_decorator.rb
@@ -1,12 +1,17 @@
+module SpreeWholesale
+  module LineItemDecorator
+    def copy_price
+      super
+      if variant
+        self.price = (order.is_wholesale? && variant.is_wholesaleable? ? variant.wholesale_price : variant.price) if price.nil?
+      end
+    end
+  end
+end
+
 Spree::LineItem.class_eval do
+  prepend SpreeWholesale::LineItemDecorator
 
   delegate_belongs_to :variant, :wholesale_price
   delegate_belongs_to :variant, :is_wholesaleable?
-
-  def copy_price
-    if variant
-      self.price = (order.is_wholesale? && variant.is_wholesaleable? ? variant.wholesale_price : variant.price) if price.nil?
-    end
-  end
-
 end
